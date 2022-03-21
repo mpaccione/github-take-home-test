ab36e67e0aaf530b375f22490d8e5ffd0e454a09
Michael Paccione <mpaccione1991@gmail.com>
2022-03-20 18:10:40 -0600

    Switch to In Memory Storage

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 // You are welcome to drop express for any other server implementation
 const express = require("express");
-const fs = require("fs");
 const uuid = require("uuid");
 
 const server = express();
 
 // The tests exercise the server by requiring it as a module,
 // rather than running it in a separate process and listening on a port
-// module.exports = server;
+module.exports = server;
 
-// We'll store the data in memory - Lol no we won't, already started with fs before noticing this xD
-// const storage = {};
+// We'll store the data in memory
+const storage = {};
 
 server.use(express.json())
 
@@ -28,48 +27,40 @@ server.use((req, res, next) => {
 // ROUTES
 
 server.get("/ping", (req, res) => {
-  res.sendStatus(200).json({ message: 'pong' })
+  res.status(200).json({ message: 'pong' })
 })
 
 server.delete("/data/:repository/:objectID", (req, res) => {
   // validate objectID
   if (!uuid.validate(req.query.objectID)) {
-    return res.sendStatus(500).json({ message: "Invalid objectID" });
+    return res.status(500).json({ message: "Invalid objectID" });
   }
 
-  fs.unlinkSync(`./data/${repository}/${objectID}.json`, (err) => {
-    if (err) {
-      return res
-        .sendStatus(404)
+  if (!storage[req.query.repository] || !storage[req.query.respository][req.query.objectId]) {
+    return res
+        .status(404)
         .json({ message: err.message ? err.message : "JSON File Note Found" });
-    }
-  });
+  } else {
+    delete storage[req.query.repository][req.query.objectId]
+  }
 
-  res.sendStatus(200).end();
+  res.status(200).end();
 });
 
 server.get("/data/:repository/:objectID", (req, res) => {
   // validate objectID
   if (!uuid.validate(req.query.objectID)) {
-    return res.sendStatus(500).json({ message: "Invalid objectID" });
+    return res.status(500).json({ message: "Invalid objectID" });
   }
 
-  let objectData;
+  // check if it exists
+  if (!storage[req.query.repository] || !storage[req.query.respository][req.query.objectId]) {
+    return res
+        .status(404)
+        .json({ message: err.message ? err.message : "JSON File Note Found" });
+  }
 
-  fs.readFileSync(
-    `./data/${repository}/${objectID}.json`,
-    "utf8",
-    (err, jsonString) => {
-      if (err) {
-        return res
-          .sendStatus(404)
-          .json({ message: err.message ? err.message : "JSON File Not Found" });
-      }
-      objectData = jsonString;
-    }
-  );
-
-  res.sendStatus(200).json(objectData);
+  res.status(200).json(storage[req.query.repository][req.query.objectID]);
 });
 
 server.put("/data/:repository", (req, res) => {
@@ -78,36 +69,16 @@ server.put("/data/:repository", (req, res) => {
     JSON.parse(req.body);
   } catch (err) {
     return res
-      .sendStatus(500)
+      .status(500)
       .json({ message: err.message ? err.message : "Invalid JSON submitted" });
   }
 
   const oid = new uuid();
-  let size;
+  const size = Buffer.byteLength(JSON.stringify(req.body), 'utf8');
 
-  // check if directory exists
-  fs.accessSync(`./data/${req.query.repository}`, function (err) {
-    // create dir if doesnt exist
-    err && fs.mkdir(`./data/${req.query.repository}`);
+  storage[req.query.repository][oid] = JSON.stringify(req.body)
 
-    // store file in repository folder
-    fs.writeFileSync(
-      `./data/${req.query.repository}/${oid}.json`,
-      JSON.stringify(req.body),
-      "utf8",
-      function (err) {
-        if (err) {
-          return res
-            .sendStatus(500)
-            .json({
-              message: err.message ? err.message : "File Could Not Be Written",
-            });
-        }
-      }
-    );
-  });
-
-  res.sendStatus(201).json({ oid, size });
+  res.status(201).json({ oid, size });
 });
 
 
